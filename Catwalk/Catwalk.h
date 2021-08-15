8410cc9eb6b0739d91cebf4caad767877becf00d
Development <development@mycatwalk.com>
2021-08-15 20:19:06 -0300

    Initial Project Setup

---
+++ Catwalk/Catwalk.h
@@ -0,0 +1,19 @@
+//
+//  Catwalk.h
+//  Catwalk
+//
+//  Created by CTWLK on 8/15/21.
+//  Copyright © 2021 CATWALK. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Catwalk.
+FOUNDATION_EXPORT double CatwalkVersionNumber;
+
+//! Project version string for Catwalk.
+FOUNDATION_EXPORT const unsigned char CatwalkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Catwalk/PublicHeader.h>
+
+
